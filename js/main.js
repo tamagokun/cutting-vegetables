8e60a7b534142ca15718dde11ea47fb14b00368a
Mike Kruk <tamagokun@gmail.com>
2014-03-20 16:22:29 -0400

    working on CSS transitions

--- js/main.js
+++ js/main.js
@@ -7,6 +7,8 @@
     // responsive videos
     $(document).fitVids();
 
+    var overlay = $('#overlay');
+    var sword = $('.sword');
 
     // cards
     $(document).on('click', '.card a', function(e) {
@@ -14,16 +16,22 @@
       e.preventDefault();
       var card = $(this).parents('.card');
       var color = card.attr('class').replace('card', '').trim();
-      $('.sword').addClass(['in', color].join(' '));
-      $('#overlay').html(card.find('.content').html()).addClass('in');
+      sword.addClass(['in', color].join(' '));
+      overlay.html( card.find('.content').html() );
+      setTimeout(function() {
+        overlay.addClass('in');
+      }, 650);
 
     });
 
     $(document).on('click', '.close', function(e) {
 
       e.preventDefault();
-      $('.sword').attr('class', 'sword');
-      $('#overlay').removeClass('in').empty();
+      sword.attr('class', 'sword');
+      overlay.attr('class', 'out');
+      setTimeout(function() {
+        overlay.empty();
+      }, 500);
 
     });
 
