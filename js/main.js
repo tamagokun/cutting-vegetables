61794be9bab58cff636506d9bd063ac7d741298e
Mike Kruk <tamagokun@gmail.com>
2014-03-20 15:57:54 -0400

    lots of progress.

--- js/main.js
+++ js/main.js
@@ -8,6 +8,25 @@
     $(document).fitVids();
 
 
+    // cards
+    $(document).on('click', '.card a', function(e) {
+
+      e.preventDefault();
+      var card = $(this).parents('.card');
+      var color = card.attr('class').replace('card', '').trim();
+      $('.sword').addClass(['in', color].join(' '));
+      $('#overlay').html(card.find('.content').html()).addClass('in');
+
+    });
+
+    $(document).on('click', '.close', function(e) {
+
+      e.preventDefault();
+      $('.sword').attr('class', 'sword');
+      $('#overlay').removeClass('in').empty();
+
+    });
+
   });
 
 })(jQuery);
